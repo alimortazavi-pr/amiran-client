7428245f2e719a49fdb80d5e6d4ca92f0583e83e
Ali Mortazavi <alimortazavi.pr@gmail.com>
2025-02-23 18:51:28 +0330

    feat: add color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,5 +10,44 @@ export default {
   ],
   theme: {},
   darkMode: "class",
-  plugins: [heroui()],
+  plugins: [
+    heroui({
+      themes: {
+        light: {
+          colors: {
+            primary: {
+              "50": "#f1fafa",
+              "100": "#dcf0f1",
+              "200": "#bde3e4",
+              "300": "#8fcdd1",
+              "400": "#5ab0b6",
+              "500": "#3f949b",
+              "600": "#377983",
+              "700": "#32646c",
+              "800": "#2f535b",
+              "900": "#2b464e",
+              DEFAULT: "#1D363D",
+            },
+          },
+        },
+        dark: {
+          colors: {
+            primary: {
+              "50": "#2b464e",
+              "100": "#2f535b",
+              "200": "#32646c",
+              "300": "#377983",
+              "400": "#3f949b",
+              "500": "#5ab0b6",
+              "600": "#8fcdd1",
+              "700": "#bde3e4",
+              "800": "#dcf0f1",
+              "900": "#f1fafa",
+              DEFAULT: "#1D363D",
+            },
+          },
+        },
+      },
+    }),
+  ],
 } satisfies Config;
