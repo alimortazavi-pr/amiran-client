10af3746f0f6d16d29a78ad0186c3bfe457b7d9c
Ali Mortazavi <alimortazavi.pr@gmail.com>
2025-06-07 11:59:33 +0330

    feat: directions

--- src/components/pages/project/DirectionsBtn.tsx
+++ src/components/pages/project/DirectionsBtn.tsx
@@ -1,25 +1,65 @@
 "use client";
 
 import { Button } from "@heroui/react";
-import { Map } from "iconsax-react";
+import { Edit, Map } from "iconsax-react";
 import { FC } from "react";
+import Link from "next/link";
 
 //Interfaces
 import { IProject } from "@/common/interfaces";
 
+//Redux
+import { useAppDispatch, useAppSelector } from "@/stores/hooks";
+import { isAuthSelector } from "@/stores/auth/selectors";
+import { setEditSection } from "@/stores/projects/actions";
+
 interface IProps {
   project: IProject;
 }
-export const DirectionsBtn: FC<IProps> = () => {
+export const DirectionsBtn: FC<IProps> = ({ project }) => {
+  //Redux
+  const dispatch = useAppDispatch();
+  const isAuth = useAppSelector(isAuthSelector);
+
+  //Functions
+  function selectSection() {
+    dispatch(
+      setEditSection({
+        label: "Directions",
+        value: "directions",
+      })
+    );
+  }
+
   return (
     <div className="flex justify-center">
-      <Button
-        aria-label="go to directions"
-        startContent={<Map className="w-5 h-5" color="white" />}
-        className="rounded-full bg-primary/30 text-default-50 w-full max-w-60 lg:max-w-80 lg:h-12 lg:text-lg"
-      >
-        Get directions
-      </Button>
+      <div className="max-w-fit relative">
+        <Button
+          aria-label="go to directions"
+          startContent={<Map className="w-5 h-5" color="white" />}
+          className="rounded-full bg-primary/30 text-default-50 w-full max-w-60 lg:max-w-80 lg:h-12 lg:text-lg"
+          as={Link}
+          href={
+            project.directions
+              ? `https://www.google.com/maps/place/${project.directions}`
+              : "#"
+          }
+          target="_blank"
+        >
+          Get directions
+        </Button>
+        {isAuth && (
+          <Button
+            isIconOnly
+            size="sm"
+            variant="light"
+            className="absolute -bottom-4 z-10"
+            onPress={selectSection}
+          >
+            <Edit className="w-5 h-5" color="#1E353C" />
+          </Button>
+        )}
+      </div>
     </div>
   );
 };
