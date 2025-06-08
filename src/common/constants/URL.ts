0999487337fbf3e7a28b0c046661a9a7f1234129
Ali Mortazavi <alimortazavi.pr@gmail.com>
2025-06-08 13:18:06 +0330

    fix: update API URLs to use production settings

--- src/common/constants/URL.ts
+++ src/common/constants/URL.ts
@@ -1,5 +1,8 @@
 // export const BASE_API_URL = "https://api.alameli.org";
 // export const SERVER_BASE_API_URL = "http://al-ameli-back:7079";
 
-export const BASE_API_URL = "http://localhost:7743";
-export const SERVER_BASE_API_URL = "http://localhost:7743";
+// export const BASE_API_URL = "http://localhost:7743";
+// export const SERVER_BASE_API_URL = "http://localhost:7743";
+
+export const BASE_API_URL = "https://amiran-api.paradisecode.ir";
+export const SERVER_BASE_API_URL = "http://amiran-back:7742";
