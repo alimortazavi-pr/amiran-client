cca71ec2db7d2bf5c6e1bf78a8ef1a3ac7de346c
Ali Mortazavi <alimortazavi.pr@gmail.com>
2025-06-18 15:17:23 +0330

    _

--- src/common/constants/URL.ts
+++ src/common/constants/URL.ts
@@ -4,5 +4,5 @@
 // export const BASE_API_URL = "http://localhost:7743";
 // export const SERVER_BASE_API_URL = "http://localhost:7743";
 
-export const BASE_API_URL = "http://171.22.25.242:7743";
-export const SERVER_BASE_API_URL = "http://amiran-back:7743";
+export const BASE_API_URL = "https://api.amirangroup.co";
+export const SERVER_BASE_API_URL = "https://api.amirangroup.co";
